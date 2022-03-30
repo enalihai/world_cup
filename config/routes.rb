3621f316cfe741e6c8d65f7b9b5b231bb1c0ed37
Luke Pascale <lpluke5@gmail.com>
2022-03-30 16:26:49 -0500

    feat: Add show and index page for leagues

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/teams', to: 'teams#index'
   get '/teams/:id', to: 'teams#show'
   get '/leagues/:league_id/teams', to: 'league_teams#index'
+  get '/leagues/', to: 'leagues#index'
+  get '/leagues/:id', to: 'leagues#show'
 end
